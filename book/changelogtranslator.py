a97d8cd6cc3fbf65fe7b2a230fb6740bd40705d2
Julien Lepiller <jlepiller@linuxfromscratch.org>
2016-09-10 10:46:17 +0000

    on met la version de développement sur le tronc
    git-svn-id: file:///home/pierre/downloads/fr-lfs-repo/trunk@7139 70be9ef0-92e7-4677-89e2-ce6697d3602d

---
+++ book/changelogtranslator.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#    Changelog Translator v0.1
+#    Traduit automatiquement certaines chaine de carractères de changelog.po
+
+#    Publié par roptat <julien@lepiller.eu> et amj <amj@linuxfromscratch.org> le 2 octobre 2015
+#    sous la licence gnu General Public License version 3 pubilée par la Free Software Foundation.
+#    Visitez <http://www.gnu.org/licenses/> pour obtenir la licence.
+
+
+
+import re
+import polib
+
+po = polib.pofile('../chapter01/changelog.po')
+
+# regexps
+simpleDateRe = re.compile('(20[0-9]{2})-([0-9]{2})-([0-9]{2})')
+simpleUpdateRe = re.compile('\[([^\]]*)\] - Update(d?) to ([^ ]*)\.')
+fixRe = re.compile('Fixes (.*).')
+
+for entry in po:
+    m = simpleDateRe.match(entry.msgid)
+    if m:
+        entry.msgstr = m.group(3) + '-' + m.group(2) + '-' + m.group(1)
+        
+        if "fuzzy" in entry.flags:
+            entry.flags.remove("fuzzy")
+        
+        print (entry.msgid + ' -> ' + entry.msgstr)
+        
+    m = simpleUpdateRe.match(entry.msgid)
+    #print(entry.msgid)
+    
+    if m:
+        entry.msgstr = '[' + m.group(1) + ']' + u' - Passage à ' + m.group(3)
+        
+        if "fuzzy" in entry.flags:
+            entry.flags.remove("fuzzy")
+        
+        m = fixRe.search(entry.msgid)
+        
+        if m:
+            entry.msgstr += ' Corrige ' + m.group(1)
+        print ("====>"+entry.msgid + ' -> ' + entry.msgstr)
+
+po.save()
