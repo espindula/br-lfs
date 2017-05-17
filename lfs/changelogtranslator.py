87f6579ef9d8e48b29517f04af3ea2aecd25cdf1
Julien Lepiller <jlepiller@linuxfromscratch.org>
2017-05-17 17:36:37 +0000

    amélioration de changelogtranslator
    git-svn-id: file:///home/pierre/downloads/fr-lfs-repo/trunk@7266 70be9ef0-92e7-4677-89e2-ce6697d3602d

--- lfs/changelogtranslator.py
+++ lfs/changelogtranslator.py
@@ -1,64 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-#    Template Translator v0.1
-#    Traduit automatiquement certaines chaine de caractères des paquets
-
-#    Publié par roptat <julien@lepiller.eu> le 8 août 2016
-#    sous la licence gnu General Public License version 3 pubilée par la Free Software Foundation.
-#    Visitez <http://www.gnu.org/licenses/> pour obtenir la licence.
-
-
 import sys
 import re
-import polib
+from templatetranslator import TemplateTranslator
 
-files = sys.argv
-files.pop(0)
+languages = sys.argv
+languages.pop(0)
+files=['chapter01/changelog.po']
 
-def convert(entry, regexp, template):
-	m = regexp.match(entry.msgid)
-	# do not modify anything if the translation is already correct
-	#if m and ("fuzzy" in entry.flags or not entry.msgstr):
-	if m:
-		msgstr = template
-		try:
-			msgstr = msgstr.replace("#1", m.group(1))
-			msgstr = msgstr.replace('#2', m.group(2))
-			msgstr = msgstr.replace('#3', m.group(3))
-			msgstr = msgstr.replace('#4', m.group(4))
-		except:
-			x=1
-		entry.msgstr = msgstr
-		if "fuzzy" in entry.flags:
-			entry.flags.remove("fuzzy")
+tt = TemplateTranslator(files)
 
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+	{'fr': '[#1] - Mise à jour vers #2. Corrige #3'})
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+). +Partially fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+	{'fr': '[#1] - Mise à jour vers #2. Corrige partiellement #3'})
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) (\([^ ]+\)). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+	{'fr': '[#1] - Mise à jour vers #2 #3. Corrige #4'})
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(([^ ]+) module\). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+	{'fr': '[#1] - Mise à jour vers #2 (module #3). Corrige #4'})
+tt.append(re.compile('(20[0-9]{2})-([0-9]{2})-([0-9]{2})'),
+	{'fr': '#3-#2-#1'})
 
-# regexps
-regexps = []
-
-regexps.append([re.compile('\[([^\]]+)\] - Updated? to ([^ ]+). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL), '[#1] - Mise à jour vers #2. Corrige #3'])
-regexps.append([re.compile('\[([^\]]+)\] - Updated? to ([^ ]+). +Partially fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL), '[#1] - Mise à jour vers #2. Corrige partiellement #3'])
-regexps.append([re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) (\([^ ]+\)). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL), '[#1] - Mise à jour vers #2 #3. Corrige #4'])
-regexps.append([re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(([^ ]+) module\). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL), '[#1] - Mise à jour vers #2 (module #3). Corrige #4'])
-
-regexps.append([re.compile('(20[0-9]{2})-([0-9]{2})-([0-9]{2})'), '#3-#2-#1'])
-
-
-
-#regexps.append([re.compile('$'), ''])
-
-number = len(files)
-current = 1
-
-for filename in files:
-	print('Traitement du fichier ', current, '/', number, '     \r',
-			end="", flush=True),
-	current = current + 1
-	po = polib.pofile(filename)
-	for entry in po:
-		for reg in regexps:
-			convert(entry, reg[0], reg[1])
-	po.save()
-print('')
-
+tt.translate(languages)
