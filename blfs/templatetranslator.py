c1fb1511d288ffed55257019cb3e2a289c752414
Julien Lepiller <jlepiller@linuxfromscratch.org>
2020-02-29 16:36:46 +0000

    Mise à jour des traductions
    git-svn-id: file:///home/pierre/downloads/fr-lfs-repo/trunk@8232 70be9ef0-92e7-4677-89e2-ce6697d3602d

--- blfs/templatetranslator.py
+++ blfs/templatetranslator.py
@@ -129,6 +129,7 @@ regexps.append([re.compile('(<.*>): This switch is used to apply( a)? higher lev
 regexps.append([re.compile('(<ulink [^>]*/>)$', re.MULTILINE|re.DOTALL), '#1'])
 regexps.append([re.compile('(<ulink [^>]*/>, )+, and (<ulink [^>]*/>)$', re.MULTILINE|re.DOTALL), '#1 et #2'])
 regexps.append([re.compile('Fix a build issue with ([^ ]+)$', re.MULTILINE|re.DOTALL), 'Corrigez un problème de construction avec #1.'])
+regexps.append([re.compile('(<othername>.*</othername> <date>.*</date>)', re.MULTILINE|re.DOTALL), '#1'])
 
 
 #regexps.append([re.compile('$'), ''])
