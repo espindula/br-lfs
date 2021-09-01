a1422e112688734fb2dd71b008e7dee00c962cc6
Pierre Labastie <pierre.labastie@neuf.fr>
2021-09-01 16:53:14 +0200

    Fix make-aux-file for lfs-bootscript entities
    now, grep returns two lines. Keep only the first one, and
    add a comment in packages;ent so that the first line is the
    one we want!

--- make-aux-files.sh
+++ make-aux-files.sh
@@ -3,7 +3,7 @@
 rm -f lfs-bootscripts*.tar.?z*
 
 # Get base file name and move bootscripts directory to that name
-version=`grep "ENTITY lfs-bootscripts-version" packages.ent |cut -d'"' -f2`
+version=`grep "ENTITY lfs-bootscripts-version" packages.ent | head -n1| cut -d'"' -f2`
 mv bootscripts lfs-bootscripts-$version
 
 # Create the tarball and clean up
