38a82278ab87e5b91966a290d19ce5efa98687d3
Julien Lepiller <jlepiller@linuxfromscratch.org>
2021-09-21 20:02:21 +0000

    Automatic LFS commit based on 9fe802c19b5fbf6c89b6fafa96e67fda12a395ab

--- lfs/epub.mk
+++ lfs/epub.mk
@@ -1,13 +1,20 @@
+ifeq ($(REV), systemd)
+ISSYSD=true
+else
+ISSYSD=
+endif
+
 epub: validate
 	@echo "Generating HTML/XML for EPUB..."
 	$(Q)if [ ! -e $(BASEDIR) ]; then \
 	        mkdir -p $(BASEDIR); \
 	fi;
-	$(Q)mkdir -p $(RENDERTMP)/lfs-epub/
-	$(Q)xsltproc --nonet --output $(RENDERTMP)/lfs-epub/ stylesheets/lfs-xsl/docbook-xsl-1.78.1/epub/docbook.xsl $(RENDERTMP)/lfs-full.xml
+	$(Q)mkdir -p $(RENDERTMP)/lfs-epub-$(REV)/
+	$(Q)xsltproc --nonet --output $(RENDERTMP)/lfs-epub-$(REV)/ stylesheets/lfs-xsl/docbook-xsl-1.78.1/epub/docbook.xsl \
+		$(RENDERTMP)/lfs-full.xml
 	@echo "Generating EPUB file..."
 	echo "application/epub+zip" > $(RENDERTMP)/mimetype
 	cwd=$$(pwd) ;\
 	cd $(RENDERTMP)/; zip -0Xq $$cwd/../$(EPUB_OUTPUT) ./mimetype
 	cwd=$$(pwd) ;\
-	cd $(RENDERTMP)/lfs-epub/; zip -Xr9Dq $$cwd/../$(EPUB_OUTPUT) ./*
+	cd $(RENDERTMP)/lfs-epub-$(REV)/; zip -Xr9Dq $$cwd/../$(EPUB_OUTPUT) ./*
