f6b4f0a0c2614a160cfe05b44ea0a92571be1125
Julien Lepiller <julien@lepiller.eu>
2023-03-12 17:20:26 +0000

    Fix fr changelogtranslator

--- lfs/changelogtranslator.py
+++ lfs/changelogtranslator.py
@@ -12,16 +12,16 @@ files=['chapter01/changelog.po']
 tt = TemplateTranslator(files)
 
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
-	{'fr': '[#1] — Mise à jour vers #2. Corrige #3',
+	{'fr': '[#1] — Mise à jour vers #2. Corrige #3.',
          'zh_CN': '[#1] — 更新到 #2。修复 #3。'})
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+). +Partially fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
-	{'fr': '[#1] — Mise à jour vers #2. Corrige partiellement #3',
+	{'fr': '[#1] — Mise à jour vers #2. Corrige partiellement #3.',
          'zh_CN': '[#1] — 更新到 #2。部分修复 #3。'})
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) (\([^ ]+\)). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
 	{'fr': '[#1] — Mise à jour vers #2 #3. Corrige #4',
          'zh_CN': '[#1] — 更新到 #2 #3。修复#4。'})
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(([^ ]+) module\). +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
-	{'fr': '[#1] — Mise à jour vers #2 (module #3). Corrige #4',
+	{'fr': '[#1] — Mise à jour vers #2 (module #3). Corrige #4.',
          'zh_CN': '[#1] — 更新到 #2（模块 #3）。修复 #4。'})
 tt.append(re.compile('(20[0-9]{2})-(0?)([0-9]+)-(0?)([0-9]+)'),
 	{'fr': '#4#5-#2#3-#1',
