9f460eae29f91a39fd66f13afcea97f963281bec
Jamenson Ferreira Espindula de Almeida Melo <jafesp@gmail.com>
2025-05-20 11:40:44 -0300

    Updates to the 'git-version.sh' file (LFS ongoing release)

--- git-version.sh
+++ git-version.sh
@@ -32,8 +32,10 @@ if ! git status > /dev/null; then
     exit 0
 fi
 
-export LC_ALL=en_US.utf8
-export TZ=America/Chicago
+#export LC_ALL=en_US.utf8
+#export TZ=America/Chicago
+export LC_ALL=pt_BR.utf8
+export TZ=America/Recife
 
 commit_date=$(git show -s --format=format:"%cd" --date=local)
 
@@ -49,7 +51,7 @@ case $day in
     * ) suffix="th";;
 esac
 
-full_date="$month $day$suffix, $year"
+full_date="$day de $month de $year"
 
 sha="$(git describe --abbrev=1)"
 rev=$(echo "$sha" | sed 's/-g[^-]*$//')
